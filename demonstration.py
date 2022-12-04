599597c38bc846ee834e17cc7d2fa79b77bfb89b
alleetanner <8190834+alleetanner@users.noreply.github.com>
2022-12-04 18:21:49 +0000

    up

--- demonstration.py
+++ demonstration.py
@@ -3,26 +3,49 @@ import pandas as pd
 import altair as alt
 
 
-def show_image():
-    with col2:
-        st.image('https://i.imgur.com/8kIWGvb.png')
-
-
+st.image("header_bar.png", use_column_width=True)
 st.header("A simple demonstration of Streamlit")
-st.subheader(f"We are currently using version {st.__version__}")
+st.info(f"We are currently using version {st.__version__}")
 
-st.write("Hi! I'm a Streamlit app!")
+st.write("We can create text like this!")
 
-col1, col2 = st.columns(2)
+st.button(
+    "Do not press this button.",
+    help="This is a button. It currently doesn't do anything.")
 
-with col1:
-    st.button("Click me!",
-            help="This is a button. Click it to see what happens.",
-            key="button1",
-            on_click=show_image)
+st.sidebar.image("generic_logo.png")
 
-st.sidebar.write("We can put stuff in the sidebar too!")
+st.sidebar.info("We can put stuff in the sidebar too!")
 
-st.sidebar.file_uploader(
+minyear = st.sidebar.slider("A range slider", 1800, 2020)
+
+# my_line = st.sidebar.radio("Select things", ["A", "B", "C"],
+my_line = st.sidebar.radio(
+    "Select things",
+    ["Buying power", "Dollar Value", "Inflation Rate"],
+    help="We use this to select items.")
+
+user_data = st.sidebar.file_uploader(
     "Upload a file",
     help="This is an upload button.")
+
+if user_data is not None:
+    # st.write("Your uploaded data looks like this:")
+    # st.dataframe(melted)
+
+    user_dataframe = pd.read_csv(user_data)
+
+    melted = pd.melt(user_dataframe, id_vars=["Year"])
+
+    years = [
+        1800, 1825, 1850, 1875,
+        1900, 1925, 1950, 1975, 2000]
+
+    chart = alt.Chart(melted).mark_line().encode(
+        x=alt.X("Year:Q", axis=alt.Axis(values=years)),
+        y=alt.Y("value:Q"),
+        color="variable:N",
+        tooltip=["Year", "value", "variable"],
+    ).interactive()
+
+    st.altair_chart(chart, use_container_width=True)
